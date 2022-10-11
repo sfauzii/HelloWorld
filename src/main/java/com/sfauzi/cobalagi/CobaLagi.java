6eb0a2f0d23aaf59a1f834814e395befc1c19fe0
S FAUZI <sjahrulfauzii@gmail.com>
2022-10-11 11:18:34 +0700

    Project Hello World

---
+++ src/main/java/com/sfauzi/cobalagi/CobaLagi.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.sfauzi.cobalagi;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class CobaLagi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Selamat Praktikum");
+        System.out.println("Pemrograman Berorientasi Objek");
+    }
+}
